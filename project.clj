2e7d301bd004fb002f233678d401fdfbde364d4a
Brian Dixon <brianide@gmail.com>
2018-08-01 22:58:07 -0500

    Cleanup and corrections

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject hlm-cv "0.1.0-SNAPSHOT"
+(defproject hot2pat "0.2.0-SNAPSHOT"
   :description "Utility for converting Hotline Miami 2 editor levels to GMS rooms"
   :url "https://github.com/brianide/hot2pat"
   :license {:name "Mozilla Public License Version 2.0"
             :url "https://www.mozilla.org/en-US/MPL/2.0/"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.xml "0.0.8"]]
-  :main hot2pat.core)
+  :main hot2pat.core
+  :profiles {:uberjar {:aot :all}})
--- src/hot2pat/core.clj
+++ src/hot2pat/core.clj
@@ -1,17 +1,20 @@
 (ns hot2pat.core
   (:require [clojure.java.io :as io])
   (:import (java.nio.file Paths)
-           (java.util Random)))
+           (java.util Random))
+  (:gen-class))
 
 (def fmt-string (str "<instance objName=\"%s\" x=\"%d\" y=\"%d\" name=\"%s\" locked=\"0\""
                      "code=\"\" scaleX=\"1\" scaleY=\"1\" colour=\"4294967295\" rotation=\"%f\"/>"))
 
 (def tab (read-string (slurp (io/resource "objtab.edn"))))
 
+
 (let [prng (Random.)]
   (defn generate-name
     ([pref] (str pref "_" (.toUpperCase (Integer/toHexString (.nextInt prng)))))
     ([] (generate-name "inst"))))
+
 
 (defn parse-obj [obj]
   (->> obj
@@ -25,6 +28,7 @@
                      :name (generate-name "obj")
                      :rot (Double/parseDouble (v 4))}))))
 
+
 (defn parse-wll [wll]
   (->> wll
        slurp
@@ -37,6 +41,7 @@
                      :y (v 2)
                      :name (generate-name "wall")
                      :rot 0.0}))))
+
 
 (defn handle-level [obj wll]
   (when (and
@@ -52,6 +57,7 @@
                        (:rot %)) insts)))
       true)))
 
+
 (defn handle-dir [dir]
   (let [root (Paths/get dir (into-array String []))
         res #(.toFile (.resolve root (format "level%d.%s" %2 %1)))
@@ -63,6 +69,6 @@
       (fn [[obj wll]] (handle-level obj wll))
       pairs)))
 
+
 (defn -main [& dirs]
-  (let [object-tab (read-string (slurp (io/resource "objtab.edn")))]
-    (doall (map handle-dir dirs))))
+  (doall (map handle-dir dirs)))
